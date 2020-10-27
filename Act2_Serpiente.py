8ed0e484d249467035e0ac5f9f75d443f01ccc4f
a00827858 <a00827858@itesm.mx>
2020-10-27 16:42:15 -0700

    Comentarios parte 2
    Documentacion parte 2

--- Act2_Serpiente.py
+++ Act2_Serpiente.py
@@ -52,26 +52,42 @@ def move():
     #Mete la nueva cabeza al final del array snake
     snake.append(head)
 
+
+
+    #Si la cabeza se come a food (estan en la misma posicion)
     if head == food:
+        #Imprime el score
         print('Snake:', len(snake))
+        #Nuevas coordenadas aleatorias de la nueva comida
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
+        
+        #Remuev el primer valor del array snake
         snake.pop(0)
 
     clear()
 
+    #Por cada pieza del vector snake (body) dibuja un cuadrado con la posicion
+    #X y Y de 9 pixeles yd e color negro
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
+    #Dibuja un cuadro para la comida con las coordenadas X y Y de 9 pixeles y de color verde
     square(food.x, food.y, 9, 'green')
+    #Actualiza la pantalla
     update()
+    #La operacion se realiza cada 100 milisegundos
     ontimer(move, 100)
-
+#Configura el tamano de la ventana y posicion de la ventana
 setup(420, 420, 370, 0)
+#Esconde el puntero
 hideturtle()
+#No dibuja el recorrido
 tracer(False)
+#Recibe las ordenes del teclado
 listen()
+#Asigna un cambio de direccion a cada tecla
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
